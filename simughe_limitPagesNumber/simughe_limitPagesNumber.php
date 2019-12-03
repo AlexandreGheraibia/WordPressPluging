5443dca16d660a671aad213a7f54ae243e9233f6
gheraibia <gheraibia@hotmail.com>
2019-12-03 20:42:06 +0100

    Add files via upload

--- simughe_limitPagesNumber/simughe_limitPagesNumber.php
+++ simughe_limitPagesNumber/simughe_limitPagesNumber.php
@@ -12,6 +12,7 @@
 */
 //include 'simurghe_options.php';
 include 'simurghe_menu.php';
+
 /*source:https://codex.wordpress.org/Writing_a_Plugin
 Security reason
 blocking direct access to the plug-in
@@ -21,8 +22,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
  class BoundsPages {
   /*
 	Apparently it's a good practice to pass by an init function.
-	for avoid the coupling between wordpress and the custom class
-	
+	For avoid the coupling between wordpress and the custom class
   */
   public static function init(){
 	   $self = new self();
@@ -38,6 +38,10 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 		if(!empty($postID)){
 		//$author=get_post($postID); 	
 		$authorID = get_current_user_id();//$author->post_author; // Post author ID.
+		/*to do
+			how retrieve my set option
+			how get the user role
+		*/
 		$max_posts = 3; // change this or set it as an option that you can retrieve.
 		$count = count_user_posts($authorID,'page',false ); // get author post count
 		if ( $count > $max_posts ) {
